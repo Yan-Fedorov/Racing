26c13a03f7202422d3dc1bcd2423b53a8af9bbd7
Yan-Fedorov <axxxx@ukr.net>
2018-03-06 13:17:33 +0200

    nothing special

--- Racing/Program.cs
+++ Racing/Program.cs
@@ -9,30 +9,12 @@ namespace Racing
     {
         static void Main(string[] args)
         {
-            //do
-            //{
+
                 Console.CursorVisible = false;
                 using (var container = IoCBuilder.Building())
                 {
                     container.Resolve<Menu>().StartMenu();
-                    //using (var scope = container.BeginLifetimeScope())
-                    //{
-
-                    //    var logica = scope.TryResolve<Logic>(out var a);
-                    //    var menu = scope.TryResolve<Menu>(out var m);
-
-                    //    var userInt = scope.TryResolve<Userinteraction>(out var b);
-
-                    //    a.backgroundGame = new Thread(m.StartMenu);
-                    //    a.backgroundGame.Start();
-                    //    a.backgroundGame.IsBackground = true;
-
-                    //    b.MoveCar();
-                    //    Console.ReadLine();
-                    //}
-
-               }
-        //} while (true);
+                }
         }
     }
 }
